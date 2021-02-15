b5e3866e60c006fe80a0a5d5027f9bc27beea318
loay bgh <loaybgh@outlook.com>
2021-02-15 20:43:38 +0100

    Add project files.

---
+++ Chat.Web/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Chat.Web.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async void Send(string user, string message)
+        {
+          await  Clients.All.SendAsync("Receive", user, message);
+        }
+    }
+}
